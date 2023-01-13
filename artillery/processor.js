7c539bd843c56be6281f01ae8634a365027453af
ildar <ildar.tamendarov@gmail.com>
2023-01-14 01:55:01 +0600

    update

--- artillery/processor.js
+++ artillery/processor.js
@@ -2,5 +2,18 @@ module.exports = {
     generateRandomId: (userContext, events, done) => {
         userContext.vars.id = Math.ceil(Math.random() * 10_000);
         return done();
+    },
+    generatePageSize: (userContext, events, done) => {
+        userContext.vars.pageSize = Math.ceil(Math.random() * 100);
+        return done();
+    },
+    generatePage: (userContext, events, done) => {
+        userContext.vars.page = Math.ceil(Math.random() * 100);
+        return done();
+    },
+    generateRandomPagination(requestParams, context, ee, next) {
+        context.vars.pageSize = Math.ceil(Math.random() * 100);
+        context.vars.page = Math.ceil(Math.random() * 100);
+        return next();
     }
 };
