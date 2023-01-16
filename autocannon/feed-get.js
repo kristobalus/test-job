e68feb2bae598ce9b8836bc2bfd0fbe0cca6f762
ildar <ildar.tamendarov@gmail.com>
2023-01-16 10:31:08 +0600

    update

--- autocannon/feed-get.js
+++ autocannon/feed-get.js
@@ -4,6 +4,7 @@ const autocannon = require('autocannon')
 const instance = autocannon({
     url: `http://${process.env.HOST}:3000`,
     duration: 60,
+    connections: 100,
     warmup: true,
     requests: [
         {
