1df35482f43f963b1368d6e9bec7bb63c522f4dc
ildar <ildar.tamendarov@gmail.com>
2023-01-17 02:06:03 +0600

    update

---
+++ autocannon/feed-get-nodepostgres.js
@@ -0,0 +1,36 @@
+
+const autocannon = require('autocannon')
+
+const instance = autocannon({
+    url: `http://${process.env.HOST}:3000`,
+    duration: 60,
+    warmup: true,
+    requests: [
+        {
+            method: 'POST',
+            title: "feed.get.nodepostgres",
+            path: '/polls/feed/get/nodepostgres',
+            setupRequest: (req, context) => {
+                req.body = JSON.stringify({
+                    "organizationId": "6911691355886452736",
+                    "eventId": 644
+                })
+                return req
+            }
+        }
+    ]
+}, finishedBench)
+
+function finishedBench (err, res) {
+    // console.log('finished bench', err, res)
+}
+
+// this is used to kill the instance on CTRL-C
+process.once('SIGINT', () => {
+    instance.stop()
+})
+
+// just render results
+autocannon.track(instance, {renderProgressBar: true, renderResultsTable: true})
+
+console.log(`benchmarking started...`)
