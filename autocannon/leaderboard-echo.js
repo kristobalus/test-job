56d166e9eb0cf722cc4b0dfdd25d69e7a698b335
ildar <ildar.tamendarov@gmail.com>
2023-01-23 09:53:49 +0600

    update

--- autocannon/leaderboard-echo.js
+++ autocannon/leaderboard-echo.js
@@ -16,10 +16,7 @@ const instance = autocannon({
             body: JSON.stringify({
                 "organizationId": "6911691355886452736",
                 "eventId": 644
-            }),
-            onResponse: (status, body, context, headers) => {
-                console.log(status, body)
-            }
+            })
         }
     ]
 }, finishedBench)
